7a973c397f1bba534e36ef6c1c609406d9616a23
vergili <vergili@cern.ch>
2014-05-24 20:41:26 -0500

    update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below 2 functions calculate inverse of a matrix
+## if inverse exist it uses cache value otherwise it calculate
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This  function includes get and set methods for given matrix, 
+## It is return a list containing functions
+## - set the value of matrix
+## - get the value of matrix
+## - set the value of inverse matrix
+## - get the value of inverse matrix
+
+makeCacheMatrix <- function(x = matrix()) {  
+  m <- NULL
+  ## Store matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## Get the matrix
+  get <- function() x
+  
+  ## Set inverse matrix
+  setinv <- function(solve) m <<- solve
+  
+  ## Get stored inverse 
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of a matrix 
+## which is created by the makeCacheMatrix function. 
+## It set this inverse via makeCacheMatrix
+## It checks this inverse before calculation if it is exist
+## it returns existing inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  ## get inverse matrix 
+  m <- x$getinv()
+  
+  ## if the inverse matrix available return it with a message  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## if inverse not available get original matrix 
+  data <- x$get()
+  
+  ## calculate inverse
+  m <- solve(data, ...)  
+  
+  ##  store inverse matrix
+  x$setinv(m)
+  
+  ## return inverse matrix
+  m
 }
+
+
+
+
+
